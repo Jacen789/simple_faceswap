2b05e93ead6a614f97cbe7a846a3139309fd4a3d
Jacen789 <Jacen789@gmail.com>
2019-01-05 00:01:53 +0800

    修改路径为绝对路径,剪切图片

--- README.md
+++ README.md
@@ -2,20 +2,20 @@
 使用opencv-python和dlib实现的简单换脸程序
 
 ## 准备 ##
-* 安装opencv-python、dlib
+* pip安装opencv-python、dlib
 * 下载dlib人脸形状检测器模型数据：[shape_predictor_68_face_landmarks.dat.bz2](http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2)，并解压在models文件夹下
 
 ## 实现步骤 ##
-	1. 使用dlib的shape_predictor_68_face_landmarks.dat模型获取人脸图片im1和摄像头图片im2的68个人脸特征点。
-	2. 根据上一步获得的特征点得到两张图片的人脸掩模im1_mask和im2_mask。
-	3. 利用68个特征点中的3个特征点，对人脸图片im1进行仿射变换使其脸部对准摄像头图片中的脸部，得到图片affine_im1。
-	4. 对人脸图片的掩模im1_mask也进行相同的仿射变换得到affine_im1_mask。
-	5. 对掩模im2_mask和掩模affine_im1_mask的掩盖部分取并集得到union_mask。
-	6. 利用opencv里的seamlessClone函数对仿射变换后的affine_im1和摄像头图片im2进行泊松融合，掩模为union_mask，得到融合后的图像seamless_im。
+1. 使用dlib的shape_predictor_68_face_landmarks.dat模型获取人脸图片im1和摄像头图片im2的68个人脸特征点。
+2. 根据上一步获得的特征点得到两张图片的人脸掩模im1_mask和im2_mask。
+3. 利用68个特征点中的3个特征点，对人脸图片im1进行仿射变换使其脸部对准摄像头图片中的脸部，得到图片affine_im1。
+4. 对人脸图片的掩模im1_mask也进行相同的仿射变换得到affine_im1_mask。
+5. 对掩模im2_mask和掩模affine_im1_mask的掩盖部分取并集得到union_mask。
+6. 利用opencv里的seamlessClone函数对仿射变换后的affine_im1和摄像头图片im2进行泊松融合，掩模为union_mask，得到融合后的图像seamless_im。
 
 ## 换脸效果 ##
 * 周杰伦的帅气照：
-![zjl.jpeg](./faces/zjl.jpeg)
+![zjl.jpeg](faces/JayChou.jpeg)
 
 * 利用杰伦的脸作为替换的脸的换脸效果：
 ![seamless_im.png](./faces/seamless_im.png)
--- faceswap.py
+++ faceswap.py
@@ -5,10 +5,12 @@ import cv2
 import dlib
 import numpy as np
 
-models_folder_path = 'models'  # 模型保存文件夹
-faces_folder_path = 'faces'  # 人脸图片保存文件夹
+here = os.path.dirname(os.path.abspath(__file__))
+
+models_folder_path = os.path.join(here, 'models')  # 模型保存文件夹
+faces_folder_path = os.path.join(here, 'faces')  # 人脸图片保存文件夹
 predictor_path = os.path.join(models_folder_path, 'shape_predictor_68_face_landmarks.dat')  # 模型路径
-image_face_path = os.path.join(faces_folder_path, 'zjl.jpeg')  # 人脸图片路径
+image_face_path = os.path.join(faces_folder_path, 'JayChou.jpeg')  # 人脸图片路径
 
 detector = dlib.get_frontal_face_detector()  # dlib的正向人脸检测器
 predictor = dlib.shape_predictor(predictor_path)  # dlib的人脸形状检测器
@@ -102,32 +104,37 @@ def get_mask_union(mask1, mask2):
     return mask
 
 
-im1 = cv2.imread(image_face_path)  # face_image
-im1 = cv2.resize(im1, (600, im1.shape[0] * 600 // im1.shape[1]))
-landmarks1 = get_face_landmarks(im1, detector, predictor)  # 68_face_landmarks
-im1_size = get_image_size(im1)  # 脸图大小
-im1_mask = get_face_mask(im1_size, landmarks1)  # 脸图人脸掩模
+def main():
+    im1 = cv2.imread(image_face_path)  # face_image
+    im1 = cv2.resize(im1, (600, im1.shape[0] * 600 // im1.shape[1]))
+    landmarks1 = get_face_landmarks(im1, detector, predictor)  # 68_face_landmarks
+    im1_size = get_image_size(im1)  # 脸图大小
+    im1_mask = get_face_mask(im1_size, landmarks1)  # 脸图人脸掩模
 
-cam = cv2.VideoCapture(0)
-while True:
-    ret_val, im2 = cam.read()  # camera_image
-    landmarks2 = get_face_landmarks(im2, detector, predictor)  # 68_face_landmarks
-    im2_size = get_image_size(im2)  # 摄像头图片大小
-    im2_mask = get_face_mask(im2_size, landmarks2)  # 摄像头图片人脸掩模
+    cam = cv2.VideoCapture(0)
+    while True:
+        ret_val, im2 = cam.read()  # camera_image
+        landmarks2 = get_face_landmarks(im2, detector, predictor)  # 68_face_landmarks
+        im2_size = get_image_size(im2)  # 摄像头图片大小
+        im2_mask = get_face_mask(im2_size, landmarks2)  # 摄像头图片人脸掩模
 
-    affine_im1 = get_affine_image(im1, im2, landmarks1, landmarks2)  # im1（脸图）仿射变换后的图片
-    affine_im1_mask = get_affine_image(im1_mask, im2, landmarks1, landmarks2)  # im1（脸图）仿射变换后的图片的人脸掩模
+        affine_im1 = get_affine_image(im1, im2, landmarks1, landmarks2)  # im1（脸图）仿射变换后的图片
+        affine_im1_mask = get_affine_image(im1_mask, im2, landmarks1, landmarks2)  # im1（脸图）仿射变换后的图片的人脸掩模
 
-    # affine_im1_face_image = cv2.bitwise_and(affine_im1, affine_im1, mask=affine_im1_mask)  # im1（脸图）的脸
-    # im2_face_image = cv2.bitwise_and(im2, im2, mask=im2_mask)  # im2（摄像头图片）的脸
-    # cv2.imshow('affine_im1_face_image', affine_im1_face_image)
-    # cv2.imshow('im2_face_image', im2_face_image)
+        # affine_im1_face_image = cv2.bitwise_and(affine_im1, affine_im1, mask=affine_im1_mask)  # im1（脸图）的脸
+        # im2_face_image = cv2.bitwise_and(im2, im2, mask=im2_mask)  # im2（摄像头图片）的脸
+        # cv2.imshow('affine_im1_face_image', affine_im1_face_image)
+        # cv2.imshow('im2_face_image', im2_face_image)
 
-    union_mask = get_mask_union(im2_mask, affine_im1_mask)  # 掩模合并
-    point = get_mask_center_point(affine_im1_mask)  # im1（脸图）仿射变换后的图片的人脸掩模的中心点
-    seamless_im = cv2.seamlessClone(affine_im1, im2, mask=union_mask, p=point, flags=cv2.NORMAL_CLONE)  # 进行泊松融合
+        union_mask = get_mask_union(im2_mask, affine_im1_mask)  # 掩模合并
+        point = get_mask_center_point(affine_im1_mask)  # im1（脸图）仿射变换后的图片的人脸掩模的中心点
+        seamless_im = cv2.seamlessClone(affine_im1, im2, mask=union_mask, p=point, flags=cv2.NORMAL_CLONE)  # 进行泊松融合
 
-    cv2.imshow('seamless_im', seamless_im)
-    if cv2.waitKey(1) == 27:
-        break
-cv2.destroyAllWindows()
+        cv2.imshow('seamless_im', seamless_im)
+        if cv2.waitKey(1) == 27:
+            break
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
